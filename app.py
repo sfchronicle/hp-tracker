bb15e90ddedb0ffb81292fe9e44ed0a063108b19
Ryan Serpico <Ryguy360@gmail.com>
2023-04-11 12:37:02 -0500

    Added service_account secret, trying to access via github secrets

--- app.py
+++ app.py
@@ -1,5 +1,41 @@
+import os
 from datetime import datetime
 
 import feedparser
 import gspread
 import pytz
+
+SERVICE_ACCOUNT = os.environ['SERVICE_ACCOUNT']
+
+# HP items
+cp = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-1-NEW-111490.php').entries[0].title
+tab2 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-2-105801.php').entries[0].title
+tab3 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-3-105803.php').entries[0].title
+tab4 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-4-105802.php').entries[0].title
+tab5 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-5-105804.php').entries[0].title
+tab6 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-6-105805.php').entries[0].title
+try:
+    breaking1 = feedparser.parse('https://www.expressnews.com/news/local/collectionRss/Breaking-Tab-1-114722.php').entries[0].title
+except:
+    breaking1 = None
+try:
+    breaking2 = feedparser.parse('https://www.expressnews.com/news/local/collectionRss/Breaking-Tab-2-115061.php').entries[0].title
+except:
+    breaking2 = None
+
+
+sa = gspread.service_account(filename=SERVICE_ACCOUNT)
+sh = sa.open('EN HP log')
+
+wks = sh.worksheet('HP Log')
+wks.insert_row([],2)
+
+wks.update_cell(2, 1, datetime.now(pytz.timezone('US/Central')).strftime('%Y-%m-%d %H:%M:%S'))
+wks.update_cell(2, 2, breaking1)
+wks.update_cell(2, 3, breaking2)
+wks.update_cell(2, 4, cp)
+wks.update_cell(2, 5, tab2)
+wks.update_cell(2, 6, tab3)
+wks.update_cell(2, 7, tab4)
+wks.update_cell(2, 8, tab5)
+wks.update_cell(2, 9, tab6)
