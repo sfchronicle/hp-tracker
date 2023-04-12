d0ffa88ad30b2bd463e6ccd6cb52cf85863c28c4
Ryan Serpico <Ryguy360@gmail.com>
2023-04-12 09:21:02 -0500

    Adapted code to scrape instead of parse RSS feeds

--- app.py
+++ app.py
@@ -1,33 +1,57 @@
 import os
 from datetime import datetime
 
-import feedparser
 import gspread
 import pytz
+import requests
+from bs4 import BeautifulSoup
 
 SERVICE_ACCOUNT = os.environ.get('SERVICE_ACCOUNT')
+
+markets = {
+    'San Antonio': {
+        'url': 'https://www.expressnews.com/',
+        'timezone': 'US/Central',
+        'spreadsheet': 'EN HP log',
+        'worksheet': 'HP Log'
+    }
+}
 
 # Create a temporary json file based on the SERVICE_ACCOUNT env variable
 with open('service_account.json', 'w') as f:
     f.write(SERVICE_ACCOUNT)
 
-def track_san_antonio():
-    # The first step is to collect HP items
-    cp = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-1-NEW-111490.php').entries[0].title
-    tab2 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-2-105801.php').entries[0].title
-    tab3 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-3-105803.php').entries[0].title
-    tab4 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-4-105802.php').entries[0].title
-    tab5 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-5-105804.php').entries[0].title
-    tab6 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-6-105805.php').entries[0].title
+def getSoup(url):
+    page = requests.get(url) 
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
+
+def get_headlines(market_url):
+    soup = getSoup(market_url)
+
+    breaking_headlines = soup.find_all('a', class_='breakingNow--item-headline')
+
+    # Find all the divs with a class of "centerpiece-tab--main-headline"
+    cp_headlines = soup.find_all('div', class_='centerpiece-tab--main-headline')
+
+    cp = cp_headlines[0].text.strip()
+    tab2 = cp_headlines[1].text.strip()
+    tab3 = cp_headlines[2].text.strip()
+    tab4 = cp_headlines[3].text.strip()
+    tab5 = cp_headlines[4].text.strip()
+    tab6 = cp_headlines[5].text.strip()
     try:
-        breaking1 = feedparser.parse('https://www.expressnews.com/news/local/collectionRss/Breaking-Tab-1-114722.php').entries[0].title
+        breaking1 = breaking_headlines[0].text.strip()
     except:
         breaking1 = None
     try:
-        breaking2 = feedparser.parse('https://www.expressnews.com/news/local/collectionRss/Breaking-Tab-2-115061.php').entries[0].title
+        breaking2 = breaking_headlines[1].text.strip()
     except:
         breaking2 = None
 
+    return breaking1, breaking2, cp, tab2, tab3, tab4, tab5, tab6
+
+def record_headlines(spreadsheet, worksheet, breaking1, breaking2, cp, tab2, tab3, tab4, tab5, tab6):
     # The second step is to authenticate with Google Sheets
     sa = gspread.service_account(filename='service_account.json')
     
@@ -51,48 +75,68 @@ def track_san_antonio():
     wks.update_cell(2, 8, tab5)
     wks.update_cell(2, 9, tab6)
 
-def track_houston():
-    cp = feedparser.parse('https://www.houstonchronicle.com/default/collectionRss/PHD-HC-Main-Centerpiece-Tab-6-101001.php').entries[0].title
-    tab2 = feedparser.parse('https://www.houstonchronicle.com/default/collectionRss/PHD-HC-Main-Centerpiece-Tab-5-101000.php').entries[0].title
-    tab3 = feedparser.parse('https://www.houstonchronicle.com/default/collectionRss/PHD-HC-Main-Centerpiece-Tab-1-103009.php').entries[0].title
-    tab4 = feedparser.parse('https://www.houstonchronicle.com/default/collectionRss/PHD-HC-Main-Centerpiece-Tab-3-98479.php').entries[0].title
-    tab5 = feedparser.parse('https://www.houstonchronicle.com/default/collectionRss/PHD-HC-Main-Centerpiece-Tab-4-98480.php').entries[0].title
-    tab6 = feedparser.parse('https://www.houstonchronicle.com/default/collectionRss/PHD-HC-Main-Centerpiece-Tab-2-98476.php').entries[0].title
+# Loop through the markets dictionary
+for market, info in markets.items():
     try:
-        breaking1 = feedparser.parse('https://www.houstonchronicle.com/default/collectionRss/Breaking-News-Tab-1-112576.php').entries[0].title
-    except:
-        breaking1 = None
-    try:
-        breaking2 = feedparser.parse('https://www.houstonchronicle.com/default/collectionRss/Breaking-News-Tab-2-112578.php').entries[0].title
-    except:
-        breaking2 = None
+        # Get the URL for the market
+        market_url = info['url']
+        # Get the headlines for the market
+        breaking1, breaking2, cp, tab2, tab3, tab4, tab5, tab6 = get_headlines(market_url)
+        # Get the spreadsheet and worksheet names
+        spreadsheet = info['spreadsheet']
+        worksheet = info['worksheet']
+        # Record the headlines
+        record_headlines(spreadsheet, worksheet, breaking1, breaking2, cp, tab2, tab3, tab4, tab5, tab6)
+    except Exception as e:
+        print(f'Error: {e}')
+        pass
 
 
-    sa = gspread.service_account(filename='service_account.json')
-    sh = sa.open('HC HP log')
+# def track_san_antonio():
+#     # The first step is to collect HP items
+#     cp = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-1-NEW-111490.php').entries[0].title
+#     tab2 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-2-105801.php').entries[0].title
+#     tab3 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-3-105803.php').entries[0].title
+#     tab4 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-4-105802.php').entries[0].title
+#     tab5 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-5-105804.php').entries[0].title
+#     tab6 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-6-105805.php').entries[0].title
+#     try:
+#         breaking1 = feedparser.parse('https://www.expressnews.com/news/local/collectionRss/Breaking-Tab-1-114722.php').entries[0].title
+#     except:
+#         breaking1 = None
+#     try:
+#         breaking2 = feedparser.parse('https://www.expressnews.com/news/local/collectionRss/Breaking-Tab-2-115061.php').entries[0].title
+#     except:
+#         breaking2 = None
 
-    wks = sh.worksheet('HP Log')
-    wks.insert_row([],2)
+#     # The second step is to authenticate with Google Sheets
+#     sa = gspread.service_account(filename='service_account.json')
+    
+#     # The third step is to open the spreadsheet we want to write to
+#     sh = sa.open('EN HP log')
 
-    wks.update_cell(2, 1, datetime.now(pytz.timezone('US/Central')).strftime('%-I:%M %p %Y-%m-%d'))
-    wks.update_cell(2, 2, breaking1)
-    wks.update_cell(2, 3, breaking2)
-    wks.update_cell(2, 4, cp)
-    wks.update_cell(2, 5, tab2)
-    wks.update_cell(2, 6, tab3)
-    wks.update_cell(2, 7, tab4)
-    wks.update_cell(2, 8, tab5)
-    wks.update_cell(2, 9, tab6)
+#     # The fourth step is to open the worksheet we want to write to
+#     wks = sh.worksheet('HP Log')
 
-try:
-    track_san_antonio()
-except:
-    pass
+#     # Step 5: Insert a row at the top of the worksheet
+#     wks.insert_row([],2)
 
-try:
-    track_houston()
-except:
-    pass
+#     # Step 6: Update the cells in the row we just inserted
+#     wks.update_cell(2, 1, datetime.now(pytz.timezone('US/Central')).strftime('%-I:%M %p %Y-%m-%d'))
+#     wks.update_cell(2, 2, breaking1)
+#     wks.update_cell(2, 3, breaking2)
+#     wks.update_cell(2, 4, cp)
+#     wks.update_cell(2, 5, tab2)
+#     wks.update_cell(2, 6, tab3)
+#     wks.update_cell(2, 7, tab4)
+#     wks.update_cell(2, 8, tab5)
+#     wks.update_cell(2, 9, tab6)
+
+
+# try:
+#     track_san_antonio()
+# except:
+#     pass
 
 # Remove the temporary json file
 os.remove('service_account.json')
