4bd825577dd173c087883c31b8f3a7d6fe8337dc
Ryan Serpico <Ryguy360@gmail.com>
2023-04-12 09:48:45 -0500

    Added Houston and removed old code

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ markets = {
         'timezone': 'US/Central',
         'spreadsheet': 'EN HP log',
         'worksheet': 'HP Log'
+    },
+    'Houston': {
+        'url': 'https://www.houstonchronicle.com/',
+        'timezone': 'US/Central',
+        'spreadsheet': 'HC HP log',
+        'worksheet': 'HP Log'
     }
 }
 
@@ -101,53 +107,6 @@ for market, info in markets.items():
     except Exception as e:
         print(f'Error: {e}')
         pass
-
-
-# def track_san_antonio():
-#     # The first step is to collect HP items
-#     cp = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-1-NEW-111490.php').entries[0].title
-#     tab2 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-2-105801.php').entries[0].title
-#     tab3 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-3-105803.php').entries[0].title
-#     tab4 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-4-105802.php').entries[0].title
-#     tab5 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-5-105804.php').entries[0].title
-#     tab6 = feedparser.parse('https://www.expressnews.com/default/collectionRss/SAEN-Homepage-Centerpiece-Tab-6-105805.php').entries[0].title
-#     try:
-#         breaking1 = feedparser.parse('https://www.expressnews.com/news/local/collectionRss/Breaking-Tab-1-114722.php').entries[0].title
-#     except:
-#         breaking1 = None
-#     try:
-#         breaking2 = feedparser.parse('https://www.expressnews.com/news/local/collectionRss/Breaking-Tab-2-115061.php').entries[0].title
-#     except:
-#         breaking2 = None
-
-#     # The second step is to authenticate with Google Sheets
-#     sa = gspread.service_account(filename='service_account.json')
-    
-#     # The third step is to open the spreadsheet we want to write to
-#     sh = sa.open('EN HP log')
-
-#     # The fourth step is to open the worksheet we want to write to
-#     wks = sh.worksheet('HP Log')
-
-#     # Step 5: Insert a row at the top of the worksheet
-#     wks.insert_row([],2)
-
-#     # Step 6: Update the cells in the row we just inserted
-#     wks.update_cell(2, 1, datetime.now(pytz.timezone('US/Central')).strftime('%-I:%M %p %Y-%m-%d'))
-#     wks.update_cell(2, 2, breaking1)
-#     wks.update_cell(2, 3, breaking2)
-#     wks.update_cell(2, 4, cp)
-#     wks.update_cell(2, 5, tab2)
-#     wks.update_cell(2, 6, tab3)
-#     wks.update_cell(2, 7, tab4)
-#     wks.update_cell(2, 8, tab5)
-#     wks.update_cell(2, 9, tab6)
-
-
-# try:
-#     track_san_antonio()
-# except:
-#     pass
 
 # Remove the temporary json file
 os.remove('service_account.json')
