af093c44b7249ad3fa59470fa0dea8931698577f
Ryan Serpico <Ryguy360@gmail.com>
2023-04-12 10:37:18 -0500

    Filled out README and updated banner

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 ![Hearst home page tracker banner](img/banner.png)
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate mi sit amet mauris. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium.
+Hey y'all!
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate mi sit amet mauris. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium.
+This bot has one simple task: Log what headlines are where on Hearst newspaper home pages every hour.
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate mi sit amet mauris. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium.
+## How it works
+
+Every hour I have a Github Action run `app.py`, which goes out to handful of Hearst newspaper sites to scrape the headlines that are present on their home pages. It then opens an existing Google Sheet tied to that market and adds the headlines. That's it!
+
+## Hearst newspapers currently tracked
+
+- [San Antonio Express-News](https://www.expressnews.com/)
+- [Houston Chronicle](https://www.houstonchronicle.com/)
+- [San Francisco Chronicle](https://www.sfchronicle.com/)
+- [Albany Times Union](https://www.timesunion.com/)
+- [Connecticut Post](https://www.ctpost.com/)
+- [Connecticut Insider](https://www.ctinsider.com/)
--- app.py
+++ app.py
@@ -7,8 +7,10 @@ import pytz
 import requests
 from bs4 import BeautifulSoup
 
+# We grab our service account from a Github secret
 SERVICE_ACCOUNT = os.environ.get('SERVICE_ACCOUNT')
 
+# We create a dictionary of the markets we want to track.
 markets = {
     'San Antonio': {
         'url': 'https://www.expressnews.com/',
@@ -53,28 +55,40 @@ with open('service_account.json', 'w') as f:
     f.write(SERVICE_ACCOUNT)
 
 def getSoup(url):
+    """
+    This function takes a URL and returns a BeautifulSoup object.
+    """
     page = requests.get(url) 
     soup = BeautifulSoup(page.content, 'html.parser')
     return soup
 
 def get_headlines(market_url):
+    """
+    This function takes a URL and returns the headlines from the homepage.
+    """
+    
+    # We use the getSoup function to get a BeautifulSoup object
     soup = getSoup(market_url)
 
+    # Find all the headlines in the breaking news bar.
     breaking_headlines = soup.find_all('a', class_='breakingNow--item-headline')
 
     # Find all the divs with a class of "centerpiece-tab--main-headline"
     cp_headlines = soup.find_all('div', class_='centerpiece-tab--main-headline')
 
-    # Certain markets, like the Albany Times Union, use a different theme for their homepage. The headlines feature a different class.
+    # Certain markets, like the Albany Times Union, use a different template for their homepage. In this scenario, we need to use a different class to find the headlines.
     if cp_headlines == []:
         cp_headlines = soup.find_all('a', class_='dynamicSpotlight--item-header')
 
+    # We extract the text from the headlines and strip the whitespace.
     cp = cp_headlines[0].text.strip()
     tab2 = cp_headlines[1].text.strip()
     tab3 = cp_headlines[2].text.strip()
     tab4 = cp_headlines[3].text.strip()
     tab5 = cp_headlines[4].text.strip()
     tab6 = cp_headlines[5].text.strip()
+
+    # We extract the text from the breaking news headlines and strip the whitespace. There isn't always a breaking news bar, so we use a try/except block to handle the error.
     try:
         breaking1 = breaking_headlines[0].text.strip()
     except:
@@ -84,19 +98,24 @@ def get_headlines(market_url):
     except:
         breaking2 = None
 
+    # Finally, we return the headlines so that we can hand them off to the next function.
     return breaking1, breaking2, cp, tab2, tab3, tab4, tab5, tab6
 
 def record_headlines(spreadsheet, worksheet, timezone, breaking1, breaking2, cp, tab2, tab3, tab4, tab5, tab6):
-    # The second step is to authenticate with Google Sheets
+    """
+    This function takes a spreadsheet name, worksheet name, timezone, and headlines and writes them to a Google Sheet.
+    """
+    
+    # We authenticate with Google using the service account json we created earlier.
     sa = gspread.service_account(filename='service_account.json')
     
-    # The third step is to open the spreadsheet we want to write to
+    # We direct the bot to the market spreadsheet we want to write to.
     sh = sa.open(spreadsheet)
 
-    # The fourth step is to open the worksheet we want to write to
+    # We direct the bot to the worksheet we want to write to.
     wks = sh.worksheet(worksheet)
 
-    # Step 5: Insert a row at the top of the worksheet
+    # We add in a new row at the top of the spreadsheet.
     wks.insert_row([],2)
 
     # Create a new variable called date and store the current date in the following format: YYYY-MM-DD
@@ -105,6 +124,7 @@ def record_headlines(spreadsheet, worksheet, timezone, breaking1, breaking2, cp,
     # Create a new variable called time and store the current time in the appropriate timezone in a 12-hour format without a leading zero
     time = datetime.now(pytz.timezone(timezone)).strftime('%-I:%M %p')
 
+    # We write the date, time, and headlines to the spreadsheet.
     wks.update_cell(2, 1, date)
     wks.update_cell(2, 2, time)
     wks.update_cell(2, 3, breaking1)
@@ -116,23 +136,30 @@ def record_headlines(spreadsheet, worksheet, timezone, breaking1, breaking2, cp,
     wks.update_cell(2, 9, tab5)
     wks.update_cell(2, 10, tab6)
 
-# Loop through the markets dictionary
+# We loop through the markets dictionary
 for market, info in markets.items():
+    print(f'🐝 Logging headlines for {market}...')
     try:
         # Get the URL for the market
         market_url = info['url']
+
         # Get the headlines for the market
         breaking1, breaking2, cp, tab2, tab3, tab4, tab5, tab6 = get_headlines(market_url)
+
         # Get the spreadsheet and worksheet names
         spreadsheet = info['spreadsheet']
         worksheet = info['worksheet']
+
+        # Get the timezone
         timezone = info['timezone']
+
         # Record the headlines
         record_headlines(spreadsheet, worksheet, timezone, breaking1, breaking2, cp, tab2, tab3, tab4, tab5, tab6)
+
     except Exception as e:
         print(f'Error: {e}')
         pass
     time.sleep(10)
 
-# Remove the temporary json file
+# Remove the temporary json file. We don't anyone to see our service account credentials!
 os.remove('service_account.json')
